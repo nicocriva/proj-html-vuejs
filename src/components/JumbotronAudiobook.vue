3ea103dd44bcd979905fd98dbfe3de8b80ab8f02
Nicolò <nicocriva1@gmail.com>
2021-07-19 11:27:50 +0200

    add sites section + first jumbo

--- src/components/JumbotronAudiobook.vue
+++ src/components/JumbotronAudiobook.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="jumbo-book">
-    jumbo audiobook
-    <!-- background img -->
-    <div>
-      <!-- img phone -->
-      <div>
-        <!-- text -->
+    <div class="container d-flex align-items-center">
+      <div class="phone-img h-100">
+        <img class="h-100 pt-5" src="../assets/audible-app-2x.png" alt="Audible app">
+      </div>
+      <div class="new-audiobook text-center">
+        <h2 class="title text-light font-times">New Audiobook</h2>
+        <div class="subtitle text-uppercase">No time to read? No problem, listen to it</div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, excepturi ratione odio?</p>
+        <a href="#"><img src="../assets/audible-logo.png" alt="Audible logo"></a>
+        <a href="#"><img src="../assets/bn-logo-tall.png" alt="Bn logo"></a>
+        <a href="#"><img src="../assets/kindle-logo-tall.png" alt="Kindle logo"></a>
       </div>
     </div>
   </div>
@@ -18,9 +23,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../style/vars.scss';
+
 .jumbo-book{
-  height: 350px;
-  background-color: #01171d;
+  height: 450px;
+  background-image: url('../assets/banner.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  .container{
+    height: 100%;
+    width: 70%;
+
+    .phone-img{
+      width: 50%;
+
+    }
+
+    .new-audiobook{
+      .title{
+        font-weight: 600;
+        font-size: 40px;
+      }
+
+      .subtitle{
+        color: $title-yellow;
+        font-weight: 600;
+      }
+
+      p{
+        font-size: 14px;
+        margin: 20px 100px;
+        color: rgba(255, 255, 255, 0.7)
+      }
+
+      img{
+        width: 120px;
+        margin: 0 20px;
+        transition: 0.5s;
+
+        &:hover{
+          transform: scale(1.05);
+        }
+      }
+    }
+  }
 }
 
 </style>
